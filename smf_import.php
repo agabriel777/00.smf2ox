e3679b175e512371ab93e35353753e2166ca089f
agabriel777 <agabriel777@gmail.com>
2014-02-03 09:47:14 +0200

    11

--- smf_import.php
+++ smf_import.php
@@ -2,7 +2,10 @@
 
 function update_users($link)
 {
+	$query = "DELETE FROM ow_base_user WHERE id >6";
+	$result = mysqli_query($link, $query);
 
+<<<<<<< HEAD
 $query = "DELETE FROM ow_base_user WHERE id >6";
 $result = mysqli_query($link, $query);
 
@@ -11,32 +14,40 @@ $result = mysqli_query($link, $query);
 
 
 $query = "select * from smf_members order by member_name";
+=======
+	$query = "DELETE FROM ow_base_question_data WHERE id >20";
+	$result = mysqli_query($link, $query);
+>>>>>>> 787f6757a596f633b7a5e022dd7ff4ca62c8f3af
 
-$result = mysqli_query($link, $query);
-    printf("Select returned %d rows.\n <br>", mysqli_num_rows($result));
+	$query = "select * from smf_members order by member_name";
+	$result = mysqli_query($link, $query);
+    
+	printf("Select returned %d rows.\n <br>", mysqli_num_rows($result));
 	echo $eol;
 	while($row = mysqli_fetch_array($result))
-  {
-   importUser($link, $row);
-  }
-   
-    /* free result set */
+	{
+		importUser($link, $row);
+	}
+     /* free result set */
     mysqli_free_result($result);
-return;
+	return; // nu e necesar
 }
 
 
 function importUser($link, $row)
 {
-include 'config.php';
-		if (!inUserDb($link, $row['member_name'])) 
+	include 'config.php'; // de ce mai e nevoie de asta?
+	if (!inUserDb($link, $row['member_name'])) 
 		{
 			echo $row['member_name'].' - insert...';
 		
 			$query = 'INSERT INTO `ow_base_user` (`email`, `username`, `password`, `joinStamp`, `activityStamp`, `accountType`, `emailVerify`, `joinIp`) VALUES ';
-			$ip = str_replace('.', '', $row['member_ip']);
-			
-			if (empty($ip)) { $ip='0'; echo "blank";}
+			//$ip = str_replace('.', '', $row['member_ip']);
+			$ip = ip2long($row['member_ip']);
+			//if (empty($ip)) { $ip='0'; echo "blank";}
+			if ($ip == -1 || $ip === FALSE) {
+				$ip=0;
+			}
 			
 			$query.= "( '".$row['email_address']."', '".$row['member_name']."', '".$row['email_address']."', ".$row['date_registered'].", ".$row['last_login'].", '290365aadde35a97f11207ca7e4279cc', 1, ".$ip.")";
 
