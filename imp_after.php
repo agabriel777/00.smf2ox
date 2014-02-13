4e9c6463c124279c241048debc0c8bda98d0b624
agabriel777 <agabriel777@gmail.com>
2014-02-13 18:11:27 +0200

    last_read_post

--- imp_after.php
+++ imp_after.php
@@ -28,19 +28,23 @@ function import_likes($link) {
 function import_last_read_post ($link) {
 global $eol;
 
-	$query = "TRUNCATE TABLE `ow_forum_read_topic`";
+   	$query = "TRUNCATE TABLE `ow_forum_read_topic`";
 	$result = mysqli_query($link, $query);
 
         echo "Import last_read_post...";
-	    $qLike= "INSERT INTO `ow_forum_read_topic` (`topicId`, `userId`, `postId`) ";
-	    $qLike.= "SELECT t.`ow_id`, u.ow_id, m.ow_id ";
-	    $qLike.= "FROM smf_members u ";
-	    $qLike.= "LEFT JOIN smf_messages m ON m.`id_msg`=u.`id_msg_last_visit` ";
-	    $qLike.= "LEFT JOIN smf_topics t ON t.`id_topic`=m.`id_topic` ";
-		$qLike.= "WHERE  id_msg_last_visit>0 ";
-	    $qLike.= "AND m.`id_msg` IS NOT NULL";
-echo $eol.$qLike.$eol;
-		$result = mysqli_query($link, $qLike);
+	    $qExec= "insert into ow_forum_read_topic (topicId, userId, postId) ";
+        $qExec.="select t.ow_id, u.ow_id, m.ow_id ";
+        $qExec.="from smf_log_topics l ";
+        $qExec.="inner join smf_messages m on m.id_msg=l.id_msg ";
+        $qExec.="inner join smf_topics t on t.id_topic = l.id_topic ";
+        $qExec.="inner join smf_members u on u.id_member=l.id_member ";
+
+		/*where u.ow_id=3
+         and t.ow_id=62
+         */
+        
+		echo $eol.$qLike.$eol;
+		$result = mysqli_query($link, $qExec);
 	    if ($result) {
 		echo "ok.";
 		}
