c3e6fb3b8fa1deee1d6c996db7e7e2bc4022368f
agabriel777 <agabriel777@gmail.com>
2014-02-15 21:35:44 +0200

    reverse update
    am pus in php 2 query-uri:
    primu importul propriu zis, deocamdata are limit 1
    si al doilea face update pe id_last_message in tabela topic

---
+++ rev_upd.php
@@ -0,0 +1,31 @@
+<?php
+
+ini_set('display_errors', 1); 
+ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+error_reporting(E_ALL);
+
+include 'config.php';
+include 'sql.php';
+
+$link = mysqli_connect($SQL_HOST.":".$SQL_PORT, $SQL_USER , $SQL_PASS , $SQL_DB);
+
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully'."<br>";
+  
+  mysqli_query($link,"SET NAMES utf8");
+
+ echo "oxwall->smf - import".$eol;
+ $result = mysqli_query($link, $qReverse);
+ if ($result) {echo "ok.";}
+ else { echo "error!"; }
+
+ echo "oxwall->smf - update last message in topic".$eol;
+ $result = mysqli_query($link, $qReverse2);
+ if ($result) {echo "ok.";}
+ else { echo "error!"; }
+
+ 
+ mysqli_close($link);
+?>
--- sql.php
+++ sql.php
@@ -32,6 +32,26 @@ $last_read_post1 .= 		"GROUP BY t.id, u.id ";
 		$last_read_post2 .= 		"INNER JOIN smf_members u ON u.`id_member`=x.uid ";
 		$last_read_post2 .= 		"WHERE m.ow_id IS NOT NULL ";
 		$last_read_post2 .= 		"    ) z ON z.tid=r.`topicId` AND z.uid=r.`userId` SET r.`postId`=z.pid WHERE r.`topicId`=z.tid  AND r.`userId`=z.uid ";
+
+		$qReverse ="";
+		$qReverse .="    INSERT INTO `vaspun`.`smf_messages` (`id_topic`,`id_board`,`poster_time`,`id_member`,`id_msg_modified`, ";
+		$qReverse .="    `subject`, `poster_name`, `poster_email`, `poster_ip`,`smileys_enabled`, ";
+		$qReverse .="    `modified_time`,`modified_name`,`body`,`icon`,`approved`,`gpbp_score`,`ow_id`) ";
+		$qReverse .="SELECT t.`id_topic`, t.`id_board`,  p.`createStamp`, u.`id_member`, 0, ";
+		$qReverse .="(SELECT `subject` FROM smf_messages WHERE id_topic = t.id_topic LIMIT 1) AS `subject`, u.`member_name`, u.`email_address`, 0, 1, ";
+		$qReverse .="0, '', p.`text`, 'xx', 1, 0, p.`id` ";
+		$qReverse .="FROM ow_forum_post p ";
+		$qReverse .="LEFT JOIN smf_topics t ON t.`ow_id` = p.`topicId` ";
+		$qReverse .="LEFT JOIN smf_members u ON u.`ow_id`=p.`userId` ";
+		$qReverse .="WHERE p.isFromImport=0 ";
+		$qReverse .="LIMIT 1";
+
+		$qReverse2 ="";
+		$qReverse2 .="UPDATE smf_topics t ";
+		$qReverse2 .="INNER JOIN (SELECT MAX(m.id_msg) maxid, m.`id_topic` FROM smf_messages m GROUP BY m.`id_topic`) z ON z.id_topic = t.`id_topic` ";
+		$qReverse2 .="SET t.`id_last_msg`= z.maxid; ";
+
+
 		
 
 ?>
