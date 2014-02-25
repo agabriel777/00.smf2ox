03989a6e48f602f2f03d2005be493472007fafc2
nyquist <nyquist@vaspun.eu>
2014-02-25 09:38:06 +0200

    indentate

--- imp_after.php
+++ imp_after.php
@@ -93,9 +93,107 @@ global $MSG_LIMIT;
 		
 	
 		
-function import_last_read_post_the_easy_way($link, $update=false){	wlog("Import last_read_post...",true);    if (!$update) 		{		/*doar la import*/		wlog('truncate table',true);		$query = "TRUNCATE TABLE `ow_forum_read_topic`";		ins($link, $query);		wlog('cross join',true);		$last_read_cross ="insert into ow_forum_read_topic (topicId, userId, postId) SELECT t.id AS tid, u.id AS uid, MAX(m.id) pid FROM ow_forum_topic t CROSS JOIN ow_base_user u INNER JOIN ow_forum_post m ON m.topicid=t.id GROUP BY t.id, u.id ";		ins($link, $last_read_cross);	}	else wlog('Running the Update Script',true);	$cond = "WHERE z.smfPid<>z.owPid";	if ($update) $cond = "WHERE z.smfPid!=z.owPid";	$ds="SELECT * FROM (		SELECT topicid AS owTid, userid AS owUid,		(SELECT id_topic FROM smf_topics WHERE ow_id=ow.topicid) AS smfTid,		(SELECT id_member FROM smf_members WHERE ow_id=ow.userid) AS smfUid,		postid AS owPid,  getLastReadInSmf(userid, topicid) AS smfPid		FROM ow_forum_read_topic ow";		/*		"-- WHERE 1=1 AND ow.userid in (2,3)". 		Nu a mers altfel! */	$ds.=") z ".$cond;    $result = mysqli_query($link, $ds);    if ($result) 	{		wlog(sprintf("*******ow_log: %d rows different.\n", mysqli_num_rows($result)),true);		while($row = mysqli_fetch_array($result)) 		{			if ($row['smfPid'] < $row['owPid']) 			{				/*am in smf necitite*/				if ($update) 				{					/* Daca e update, atunci inseamna ca au fost citite in OW => Update SMF	*/					SMF_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);				}				else 				{					/* Daca e import, atunci inseamna ca sunt necitite in SMF => Update in OW*/					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);				}			} 			else 						{				if ($update) 				{					/* Daca e update, atunci inseamna ca au fost citite in SMF => Update in OW	*/					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);				}				else				{										/*					Daca e import, n-ar trebui sa faca nimic					probabil nu mai era cazul, dar ca sa nu iasa vreo 	belea :)					*/				}
-			}/*if < */		}/*while*/	}/*if result*/	else	{		wlog('No result for '.$ds,true);	}}
-function SMF_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID){		$q = "select count(*) cnt from smf_log_topics where id_member=".$SMF_User." and id_topic=".$SMF_Topic;	$result2 = mysqli_query($link, $q);	if ($result2) 	{				$row2 = mysqli_fetch_array($result2);		if ($row2['cnt']==0) 		{			/*insert smf*/			wlog("insert SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);			$q="INSERT INTO smf_log_topics (id_member, id_topic, id_msg) VALUES (".$SMF_User.", ".$SMF_Topic.", (select id_msg from smf_messages where ow_id=".$OW_PID.") )";			ins($link,$q);		}		else 		{			wlog("update SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);			$q="UPDATE smf_log_topics SET id_msg=(select id_msg from smf_messages where ow_id=".$OW_PID.") where id_member=".$SMF_User." and id_topic=".$SMF_Topic;			$k=upd_log($link, $q);			$k=0;			wlog(sprintf("%d rows updated",$k),true);		}		} /*if result2*/}function OW_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID){	/*wlog("update OW (U-T): ow=".$row['owUid']."-".$row['owTid'].", smf=".$row['smfUid']."-".$row['smfTid'], true);*/	wlog("update OW (U-T): ow=".$OW_User."-".$OW_Topic.", smf=".$SMF_User."-".$SMF_Topic, true);	/*$q = "UPDATE ow_forum_read_topic set postId = ".$row['smfPid']." where userId=".$row['owUid']." and topicId = ".$row['owTid'];*/	$q = "UPDATE ow_forum_read_topic set postId = ".$SMF_PID." where userId=".$OW_User." and topicId = ".$OW_Topic;	$k=upd_log($link, $q);	/*if ($k!=0) wlog($row['owPid']."->".$row['smfPid'], true);*/	if ($k!=0) wlog("Topic Read in SMF. Updated OXWALL for U-T:".$OW_User."-".$OW_Topic.". OW_PID:".$OW_PID."|SMF Pid:".$SMF_PID."", true);}			
-		
+function import_last_read_post_the_easy_way($link, $update=false)
+{
+	wlog("Import last_read_post...",true);
+	if (!$update
+	{
+		/*doar la import*/
+		wlog('truncate table',true);
+		$query = "TRUNCATE TABLE `ow_forum_read_topic`";
+		ins($link, $query);		wlog('cross join',true);
+		$last_read_cross ="insert into ow_forum_read_topic (topicId, userId, postId) SELECT t.id AS tid, u.id AS uid, MAX(m.id) pid FROM ow_forum_topic t CROSS JOIN ow_base_user u INNER JOIN ow_forum_post m ON m.topicid=t.id GROUP BY t.id, u.id ";
+		ins($link, $last_read_cross);
+	}
+	else wlog('Running the Update Script',true);
+	$cond = "WHERE z.smfPid<>z.owPid";	if ($update) $cond = "WHERE z.smfPid!=z.owPid";
+	$ds="SELECT * FROM (
+		SELECT topicid AS owTid, userid AS owUid,
+		(SELECT id_topic FROM smf_topics WHERE ow_id=ow.topicid) AS smfTid,
+		(SELECT id_member FROM smf_members WHERE ow_id=ow.userid) AS smfUid,
+		postid AS owPid,  getLastReadInSmf(userid, topicid) AS smfPid
+		FROM ow_forum_read_topic ow";
+		/*		"-- WHERE 1=1 AND ow.userid in (2,3)". 		Nu a mers altfel! */
+		$ds.=") z ".$cond;
+		$result = mysqli_query($link, $ds);
+	if ($result)
+	{		
+		wlog(sprintf("*******ow_log: %d rows different.\n", mysqli_num_rows($result)),true);
+		while($row = mysqli_fetch_array($result))
+		{
+			if ($row['smfPid'] < $row['owPid'])
+			{
+				/*am in smf necitite*/
+				if ($update)
+				{
+					/* Daca e update, atunci inseamna ca au fost citite in OW => Update SMF	*/
+					SMF_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+				else
+				{
+					/* Daca e import, atunci inseamna ca sunt necitite in SMF => Update in OW*/
+					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+			}
+			else
+			{
+				if ($update)
+				{
+					/* Daca e update, atunci inseamna ca au fost citite in SMF => Update in OW	*/
+					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+				else
+				{
+					/*
+					Daca e import, n-ar trebui sa faca nimic
+					probabil nu mai era cazul, dar ca sa nu iasa vreo 	belea :)
+					*/
+				}
+			}/*if < */
+		}/*while*/
+	}/*if result*/
+	else	
+	{		
+		wlog('No result for '.$ds,true);
+	}
+}
+
+
+function SMF_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID)
+{
+
+	$q = "select count(*) cnt from smf_log_topics where id_member=".$SMF_User." and id_topic=".$SMF_Topic;
+	$result2 = mysqli_query($link, $q);
+	if ($result2)
+ 	{
+		$row2 = mysqli_fetch_array($result2);
+		if ($row2['cnt']==0)
+		{			
+			/*insert smf*/
+			wlog("insert SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);
+			$q="INSERT INTO smf_log_topics (id_member, id_topic, id_msg) VALUES (".$SMF_User.", ".$SMF_Topic.", (select id_msg from smf_messages where ow_id=".$OW_PID.") )";
+			ins($link,$q);
+		}
+		else
+ 		{
+			wlog("update SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);
+			$q="UPDATE smf_log_topics SET id_msg=(select id_msg from smf_messages where ow_id=".$OW_PID.") where id_member=".$SMF_User." and id_topic=".$SMF_Topic;
+			$k=upd_log($link, $q);
+			$k=0;
+			wlog(sprintf("%d rows updated",$k),true);
+		}
+	} /*if result2*/
+}
+
+function OW_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID)
+{
+	/*wlog("update OW (U-T): ow=".$row['owUid']."-".$row['owTid'].", smf=".$row['smfUid']."-".$row['smfTid'], true);*/
+	wlog("update OW (U-T): ow=".$OW_User."-".$OW_Topic.", smf=".$SMF_User."-".$SMF_Topic, true);
+	/*$q = "UPDATE ow_forum_read_topic set postId = ".$row['smfPid']." where userId=".$row['owUid']." and topicId = ".$row['owTid'];*/
+	$q = "UPDATE ow_forum_read_topic set postId = ".$SMF_PID." where userId=".$OW_User." and topicId = ".$OW_Topic;	$k=upd_log($link, $q);
+	/*if ($k!=0) wlog($row['owPid']."->".$row['smfPid'], true);*/
+	if ($k!=0) wlog("Topic Read in SMF. Updated OXWALL for U-T:".$OW_User."-".$OW_Topic.". OW_PID:".$OW_PID."|SMF Pid:".$SMF_PID."", true);
+}
+
 
 ?>
