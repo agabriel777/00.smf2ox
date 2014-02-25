257218c906a669773579c4bd68dca3f822c53a61
agabriel777 <agabriel777@gmail.com>
2014-02-25 09:57:32 +0200

    2

--- imp_after.php
+++ imp_after.php
@@ -1,6 +1,6 @@
 <?php
 
-function update_last_reply ($link,$update = false) {
+function update_last_reply ($link,$update = false) {  //OW last reply in topic
         $q_update_last_reply = "UPDATE ow_forum_topic t SET t.lastPostId = (SELECT MAX(id) FROM ow_forum_post p WHERE p.topicId=t.id)";
 
         wlog("update last_reply...",true);
@@ -8,15 +8,16 @@ function update_last_reply ($link,$update = false) {
 }
 
 function import_likes($link,$update=false) {//import thanks
+
 $qLikes = "INSERT INTO `ow_newsfeed_like` (`entityType`,`entityId`,`userId`,`timeStamp`) 
-           SELECT 'forum-post', m.`ow_id`, u.`ow_id`, g.`log_time`
-  	       FROM smf_log_gpbp g 
-           INNER JOIN smf_messages m ON m.`id_msg` = g.`id_msg` 
-           INNER JOIN smf_members u ON u.`id_member`=g.`id_member` 
-           LEFT JOIN ow_newsfeed_like n ON n.entityID=m.ow_id AND n.userid=u.ow_id
-           WHERE 1=1 -- m.ow_id IS NOT NULL
-           AND n.id IS NULL
-           AND g.score>0
+			   SELECT 'forum-post', m.`ow_id`, u.`ow_id`, g.`log_time`
+			   FROM smf_log_gpbp g 
+			   INNER JOIN smf_messages m ON m.`id_msg` = g.`id_msg` 
+			   INNER JOIN smf_members u ON u.`id_member`=g.`id_member` 
+			   LEFT JOIN ow_newsfeed_like n ON n.entityID=m.ow_id AND n.userid=u.ow_id
+			   WHERE 1=1 -- m.ow_id IS NOT NULL
+			   AND n.id IS NULL
+			   AND g.score>0
 ";
 
 
@@ -40,9 +41,10 @@ $qLikes = "INSERT INTO `ow_newsfeed_like` (`entityType`,`entityId`,`userId`,`tim
 function ow2smf($link) {	 
 global $MSG_LIMIT;
 		
-		
+		//update SMF last_message in topic
 		$qReverse2 = "UPDATE smf_topics t INNER JOIN (SELECT MAX(m.id_msg) maxid, m.`id_topic` FROM smf_messages m GROUP BY m.`id_topic`) z ON z.id_topic = t.`id_topic` SET t.`id_last_msg`= z.maxid "; 
 
+		//update SMF max_msg_id
 		$qReverse3 ="UPDATE smf_settings s SET `value`=(SELECT MAX(id_msg) FROM smf_messages) WHERE variable = 'maxMsgID'";
 
 		
@@ -75,24 +77,6 @@ global $MSG_LIMIT;
 
 			     $q = "UPDATE smf_log_topics set id_msg = ".$ins_id." where id_member = ".$row['id_member']." and id_topic=".$row['id_topic'];
 				ins($link, $q);
-
-				
-				/* --scos ca se face la sincronizare
-				$q = "SELECT id_msg from smf_log_topics where id_member = ".$row['id_member']." and id_topic = ".$row['id_topic'];
-				$result2 =  mysqli_query($link, $q);
-                if ($row2=mysqli_fetch_array($result2)) {//am linie
-				    wlog("id existent = ".$row2['id_msg'],true);
-					wlog("id nou = ".$ins_id, true);
-				    wlog("fac update in smf_log_topics",true);
-  			        $q = "UPDATE smf_log_topics SET id_msg=".$ins_id." where id_member=".$row['id_member']." and id_topic=".$row['id_topic']." and id_msg<".$ins_id;
-				}
-				else
-				{
-                    wlog("inserez in smf_log_topics",true);
-				    $q = "INSERT INTO smf_log_topics (id_member, id_topic, id_msg) VALUES (".$row['id_member'].", ".$row['id_topic'].", ".$ins_id.")";
-					ins($link, $q);
-				}
-				*/
 				
 			}
  
@@ -107,38 +91,74 @@ global $MSG_LIMIT;
  }
  
 		
-function import_last_read_post ($link, $update=false) {
-// toate topicurile pt toti userii
-$last_read_post1 ="insert into ow_forum_read_topic (topicId, userId, postId) SELECT t.id AS tid, u.id AS uid, MAX(m.id) pid FROM ow_forum_topic t CROSS JOIN ow_base_user u INNER JOIN ow_forum_post m ON m.topicid=t.id GROUP BY t.id, u.id ";
-//omise topicurile din grupul 
-
-
-
-$last_read_post2 ="UPDATE ow_forum_read_topic r 
-                   INNER JOIN (SELECT t.ow_id AS tid, u.ow_id AS uid, m.ow_id AS pid  FROM 
-								( SELECT  l.id_topic AS tid, l.id_member AS uid, 
-										(SELECT MAX(id_msg) FROM smf_messages m2 WHERE m2.id_msg<=l.id_msg AND m2.id_topic=l.`id_topic`) AS pid 
-										FROM smf_log_topics l 
-								) AS x 
-								INNER JOIN smf_messages m ON m.`id_msg`=x.pid 
-								INNER JOIN smf_topics t ON t.`id_topic` = x.tid 
-								INNER JOIN smf_members u ON u.`id_member`=x.uid 
-								WHERE m.ow_id IS NOT NULL 
-							) z ON z.tid=r.`topicId` AND z.uid=r.`userId` SET r.`postId`=z.pid WHERE r.`topicId`=z.tid  AND r.`userId`=z.uid ";
-
-		 
-   	$query = "TRUNCATE TABLE `ow_forum_read_topic`";
-    ins($link, $query);
-
-        wlog("Import last_read_post...",true);
+	
 		
-		ins($link, $last_read_post1);
-		
-		ins($link, $last_read_post2);
-		
-} 		
-		
+function import_last_read_post_the_easy_way($link, $update=false)
+{
+	wlog("Import last_read_post...",true);
+	if (!$update
+	{
+		/*doar la import*/
+		wlog('truncate table',true);
+		$query = "TRUNCATE TABLE `ow_forum_read_topic`";
+		ins($link, $query);		wlog('cross join',true);
+		$last_read_cross ="insert into ow_forum_read_topic (topicId, userId, postId) SELECT t.id AS tid, u.id AS uid, MAX(m.id) pid FROM ow_forum_topic t CROSS JOIN ow_base_user u INNER JOIN ow_forum_post m ON m.topicid=t.id GROUP BY t.id, u.id ";
+		ins($link, $last_read_cross);
+	}
+	else wlog('Running the Update Script',true);
+	$cond = "WHERE z.smfPid<>z.owPid";	if ($update) $cond = "WHERE z.smfPid!=z.owPid";
+	$ds="SELECT * FROM (
+		SELECT topicid AS owTid, userid AS owUid,
+		(SELECT id_topic FROM smf_topics WHERE ow_id=ow.topicid) AS smfTid,
+		(SELECT id_member FROM smf_members WHERE ow_id=ow.userid) AS smfUid,
+		postid AS owPid,  getLastReadInSmf(userid, topicid) AS smfPid
+		FROM ow_forum_read_topic ow";
+		/*		"-- WHERE 1=1 AND ow.userid in (2,3)". 		Nu a mers altfel! */
+		$ds.=") z ".$cond;
+		$result = mysqli_query($link, $ds);
+	if ($result)
+	{		
+		wlog(sprintf("*******ow_log: %d rows different.\n", mysqli_num_rows($result)),true);
+		while($row = mysqli_fetch_array($result))
+		{
+			if ($row['smfPid'] < $row['owPid'])
+			{
+				/*am in smf necitite*/
+				if ($update)
+				{
+					/* Daca e update, atunci inseamna ca au fost citite in OW => Update SMF	*/
+					SMF_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+				else
+				{
+					/* Daca e import, atunci inseamna ca sunt necitite in SMF => Update in OW*/
+					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+			}
+			else
+			{
+				if ($update)
+				{
+					/* Daca e update, atunci inseamna ca au fost citite in SMF => Update in OW	*/
+					OW_MarkRead($link, $row['owUid'], $row['smfUid'], $row['owTid'], $row['smfTid'], $row['owPid'], $row['smfPid']);
+				}
+				else
+				{
+					/*
+					Daca e import, n-ar trebui sa faca nimic
+					probabil nu mai era cazul, dar ca sa nu iasa vreo 	belea :)
+					*/
+				}
+			}/*if < */
+		}/*while*/
+	}/*if result*/
+	else	
+	{		
+		wlog('No result for '.$ds,true);
+	}
+}
 
+<<<<<<< HEAD
 		
 		
 function import_last_read_post_the_easy_way($link, $update=false)		 {
@@ -200,9 +220,44 @@ function import_last_read_post_the_easy_way($link, $update=false)		 {
 			
 		  }//while
       }//if result
+=======
+
+function SMF_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID)
+{
+
+	$q = "select count(*) cnt from smf_log_topics where id_member=".$SMF_User." and id_topic=".$SMF_Topic;
+	$result2 = mysqli_query($link, $q);
+	if ($result2)
+ 	{
+		$row2 = mysqli_fetch_array($result2);
+		if ($row2['cnt']==0)
+		{			
+			/*insert smf*/
+			wlog("insert SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);
+			$q="INSERT INTO smf_log_topics (id_member, id_topic, id_msg) VALUES (".$SMF_User.", ".$SMF_Topic.", (select id_msg from smf_messages where ow_id=".$OW_PID.") )";
+			ins($link,$q);
+		}
+		else
+ 		{
+			wlog("update SMF (U-T): ".$SMF_User."-".$SMF_Topic, true);
+			$q="UPDATE smf_log_topics SET id_msg=(select id_msg from smf_messages where ow_id=".$OW_PID.") where id_member=".$SMF_User." and id_topic=".$SMF_Topic;
+			$k=upd_log($link, $q);
+			$k=0;
+			wlog(sprintf("%d rows updated",$k),true);
+		}
+	} /*if result2*/
+>>>>>>> 03989a6e48f602f2f03d2005be493472007fafc2
 }
-		
-	
-		
+
+function OW_MarkRead($link, $OW_User, $SMF_User, $OW_Topic, $SMF_Topic, $OW_PID, $SMF_PID)
+{
+	/*wlog("update OW (U-T): ow=".$row['owUid']."-".$row['owTid'].", smf=".$row['smfUid']."-".$row['smfTid'], true);*/
+	wlog("update OW (U-T): ow=".$OW_User."-".$OW_Topic.", smf=".$SMF_User."-".$SMF_Topic, true);
+	/*$q = "UPDATE ow_forum_read_topic set postId = ".$row['smfPid']." where userId=".$row['owUid']." and topicId = ".$row['owTid'];*/
+	$q = "UPDATE ow_forum_read_topic set postId = ".$SMF_PID." where userId=".$OW_User." and topicId = ".$OW_Topic;	$k=upd_log($link, $q);
+	/*if ($k!=0) wlog($row['owPid']."->".$row['smfPid'], true);*/
+	if ($k!=0) wlog("Topic Read in SMF. Updated OXWALL for U-T:".$OW_User."-".$OW_Topic.". OW_PID:".$OW_PID."|SMF Pid:".$SMF_PID."", true);
+}
+
 
 ?>
